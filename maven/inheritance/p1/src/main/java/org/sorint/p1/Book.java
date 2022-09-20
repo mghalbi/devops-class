6675ac308c45bd302bd35cb8df54307ef35c565f
mghalbi <mohsenghalbi@gmail.com>
2022-09-20 21:49:37 +0200

    Maven example.

---
+++ maven/inheritance/p1/src/main/java/org/sorint/p1/Book.java
@@ -0,0 +1,27 @@
+package org.sorint.p1;
+
+import  com.google.gson.Gson;
+
+
+public class Book {
+
+    private String author;
+
+    private  String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ maven/inheritance/p2/src/main/java/org/sorint/p2/Book.java
@@ -0,0 +1,26 @@
+package org.sorint.p2;
+
+import  com.google.gson.Gson;
+
+public class Book {
+
+    private String author;
+
+    private  String title;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
